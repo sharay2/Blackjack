72e86a8cb5d6d9e7a8ea43468d7910e566ece88a
Stephen Harayo <sharay2@uic.edu>
2024-03-14 15:56:48 -0500

    3:56PM - BlackjackDealer.java complete

--- src/main/java/BlackjackDealer.java
+++ src/main/java/BlackjackDealer.java
@@ -1,24 +1,52 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BlackjackDealer{
+    ArrayList<Card> deck; //where all the cards will be, index 0 being the top of the deck, and the last index being the bottom of the deck
     public void generateDeck() {
+        deck = new ArrayList<>();
 
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        int[] values = {1,2,3,4,5,6,7,8,9,10,11,12,13}; //1 = ace, 11 = jack, 12 = queen, 13 = king, rest are normal values
+
+        for(String suit:suits){ //double for loop that will combine each value with each suit
+            for(int value: values){
+                deck.add(new Card(suit,value));
+            }
+        }
     }
 
     public ArrayList<Card> dealHand(){
-        return new ArrayList<Card>();
+        if(deck.isEmpty()){ //making sure that the deck was generated correctly before dealing hands
+            generateDeck();
+        }
+
+        ArrayList<Card> dealerCards = new ArrayList<>(); //the arraylist that will hold on to the two cards given to the player/dealer
+        dealerCards.add(deck.get(0)); //grabs the first two cards from the deck and stores it in the new arraylist
+        dealerCards.add(deck.get(1));
+        //removes the first two cards from the deck
+        deck.remove(0);
+        deck.remove(0);
+        //returns the two cards grabbed from the deck
+        return dealerCards;
     }
 
     public Card drawOne(){
-        return new Card("peepee",1);
+        if(deck.isEmpty()){ //making sure that the deck was generated correctly before drawing one card
+            generateDeck();
+        }
+
+        Card topcard = deck.get(0); //grabs a card from the top of the deck (given that the deck is already shuffled)
+        deck.remove(0); //removes that card from the deck (removes first index)
+        return topcard; //returns the card
     }
 
     public void shuffleDeck(){
-
+        Collections.shuffle(deck); //uses Collections.shuffle to randomize the deck
     }
 
     public int deckSize(){
-        return 52;
+        return deck.size(); //returns the size of the deck
     }
 }
 
